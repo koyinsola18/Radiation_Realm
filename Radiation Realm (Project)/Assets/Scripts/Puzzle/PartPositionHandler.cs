603e933d69f46624b6fecbdfa2075f64bb4606cf
Daniel Akanmu <akanmu@ualberta.ca>
2023-10-26 22:17:49 -0600

    pushed project

---
+++ Radiation Realm (Project)/Assets/Scripts/Puzzle/PartPositionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PartPositionHandler : MonoBehaviour, IDropHandler
+{
+	[SerializeField] AudioSource audioSource;
+	[SerializeField] AudioClip[] clips;
+    public int id;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Item Dropped");
+		if (eventData.pointerDrag != null)
+		{
+			if (eventData.pointerDrag.GetComponent<PartHandler>().id == id)
+			{
+				eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = 
+						this.GetComponent<RectTransform>().anchoredPosition;
+					audioSource.PlayOneShot(clips[0]);
+			}
+			else
+			{
+				eventData.pointerDrag.GetComponent<PartHandler>().ResetPosition();
+			}
+		}
+    }
+}
---
+++ Radiation Realm (Project)/Assets/Scripts/Wire Puzzle/WirePuzzleHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WirePuzzleHandler : MonoBehaviour
+{
+    [SerializeField] GameObject wirePuzzle;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        wirePuzzle.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        wirePuzzle.SetActive(false);
+    }
+}
