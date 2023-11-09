019494f712334443ef173d667a61991197ef373f
aalpesh <aalpeshdayal@gmail.com>
2023-11-09 01:02:24 -0700

    Room navigation and bug fixing
    List of changes:
    - moved radiation bar to correct spot (top right) and added border as recommended by prof
    
    - made room navigation so now other programmers can go in between the correct rooms
    
    - started level design on landfill level
    
    - added barks to landfill level
    
    - assigned game objects to missing instances in scenes
    
    - moved player to leftmost of levels
    - rewrote the level manager script
    
    known bugs:
    - radiation bar resets after each scene load

--- Radiation Realm MAIN PROJECT/Assets/Scripts/Managers/LevelManager.cs
+++ Radiation Realm MAIN PROJECT/Assets/Scripts/Managers/LevelManager.cs
@@ -8,14 +8,29 @@ public class LevelManager : MonoBehaviour
     [SerializeField] GameObject loadingScreen;
     [SerializeField] GameObject radiationBar;
 
+    // Public variable to set the scene name in the Unity Inspector
+    public string sceneToLoad;
+
     void Start()
     {
         loadingScreen.SetActive(false);
     }
-    void OnTriggerEnter2D()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        radiationBar.SetActive(false);
-        loadingScreen.SetActive(true);
-        SceneManager.LoadScene("Level_1");
+        if (other.CompareTag("Player"))
+        {
+            radiationBar.SetActive(false);
+            loadingScreen.SetActive(true);
+
+            // Call the LoadScene method with the specified scene name
+            LoadScene(sceneToLoad);
+        }
+    }
+
+    // This is the LoadScene method outside of the OnTriggerEnter2D
+    void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
