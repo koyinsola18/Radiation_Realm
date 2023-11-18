806c223074a322daf348531a4ddad55470161b52
Aalpesh <aalpeshdayal@gmail.com>
2023-11-17 21:37:53 -0500

    Added base scene
    Wrote win scene, implemented in game, made gateway from landfill to base

---
+++ Radiation Realm MAIN PROJECT/Assets/textscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarWarsCrawl : MonoBehaviour
+{
+    public Text crawlText;
+    public float scrollSpeed = 20f;
+
+    void Start()
+    {
+        InitializeCrawl();
+    }
+
+    void Update()
+    {
+        ScrollCrawl();
+    }
+
+    void InitializeCrawl()
+    {
+        crawlText.text = "A long time ago in a galaxy far, far away....\nYour text goes here. Add more lines as needed.";
+    }
+
+    void ScrollCrawl()
+    {
+        Vector3 pos = crawlText.transform.position;
+        pos.y += scrollSpeed * Time.deltaTime;
+        crawlText.transform.position = pos;
+    }
+}
