e844c27b1e3a5ff988c3c4c18398017d7e374615
koyinsola18 <koyinsolatitiloye@gmail.com>
2023-11-19 18:07:03 -0700

    Pipe puzzle and septic puzzle updated

---
+++ Radiation Realm MAIN PROJECT/Assets/Pipe puzzle/GameManager2.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager2 : MonoBehaviour
+{
+    public GameObject PipesHolder;
+    public Pipescript2[] Pipes;
+
+    [SerializeField]
+    int totalPipes = 0;
+    [SerializeField]
+    int correctedPipes = 0;
+
+    public Text WinText;
+
+    void Start()
+    {
+        WinText.gameObject.SetActive(false);
+        Pipes = PipesHolder.GetComponentsInChildren<Pipescript2>();
+        totalPipes = Pipes.Length;
+
+        foreach (Pipescript2 pipe in Pipes)
+        {
+            Button pipeButton = pipe.GetComponent<Button>();
+            pipeButton.onClick.AddListener(pipe.RotatePipe);
+        }
+    }
+
+    public void CorrectMove()
+    {
+        correctedPipes += 1;
+
+        Debug.Log("Correct Move");
+
+        if (correctedPipes == totalPipes)
+        {
+            Debug.Log("You win!");
+            WinText.gameObject.SetActive(true);
+        }
+    }
+
+    public void WrongMove()
+    {
+        correctedPipes -= 1;
+    }
+}
+
---
+++ Radiation Realm MAIN PROJECT/Assets/Pipe puzzle/Pipescript2.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pipescript2 : MonoBehaviour
+{
+    float[] rotations = { 0, 90, 180, 270 };
+
+    public float[] correctRotation;
+    [SerializeField]
+    bool isPlaced = false;
+
+    int PossibleRots = 1;
+
+    GameManager2 gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager2").GetComponent<GameManager2>();
+    }
+
+    private void Start()
+    {
+        PossibleRots = correctRotation.Length;
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+
+        if (PossibleRots > 1)
+        {
+            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1])
+            {
+                isPlaced = true;
+                gameManager.CorrectMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.z == correctRotation[0])
+            {
+                isPlaced = true;
+                gameManager.CorrectMove();
+            }
+        }
+    }
+
+    public void RotatePipe()
+    {
+        if (!isPlaced)
+        {
+            transform.Rotate(new Vector3(0, 0, 90));
+
+            if (PossibleRots > 1)
+            {
+                if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1])
+                {
+                    isPlaced = true;
+                    gameManager.CorrectMove();
+                }
+                else if (isPlaced)
+                {
+                    isPlaced = false;
+                    gameManager.WrongMove();
+                }
+            }
+            else
+            {
+                if (transform.eulerAngles.z == correctRotation[0])
+                {
+                    isPlaced = true;
+                    gameManager.CorrectMove();
+                }
+                else if (isPlaced)
+                {
+                    isPlaced = false;
+                    gameManager.WrongMove();
+                }
+            }
+        }
+    }
+}
